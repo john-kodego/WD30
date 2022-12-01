3bfc2bbf62cbbbdd63fa59d25c09003e51ef3f6a
John Macaday <john@kodego.ph>
2022-12-01 14:47:17 +0800

    intro to javascript

---
+++ JS/12012022/index.js
@@ -0,0 +1,71 @@
+// Display Functions
+// alert("Hello Philippines!");
+// console.log("Hello Console!");
+
+// Variables
+
+var person = "John"; // string data type / string literal
+// console.log(person);
+
+let age = 12; //number data type
+age = 15;
+// console.log(age);
+
+const birthplace = "Quezon City";
+// console.log(birthplace);
+
+// Data Types
+const firstName = "Jose"; // String Data type, String literal
+const lastName = 'Rizal';
+// backtick
+const message = `Jose Rizal
+                                this is another line
+one more line`; // String Data type, Template literal
+const grade = 100; // Number Data type
+const average = 95.6;
+const passed = true; // Boolean Data type
+const value = null; // Null
+const unDef = undefined; // undefined
+
+// console.log( firstName );
+// console.log( typeof firstName );
+
+// console.log( typeof message );
+
+// console.log( grade );
+// console.log( typeof grade );
+
+// console.log( average );
+// console.log( typeof average );
+
+// console.log( passed );
+// console.log( typeof passed );
+
+// console.log( value );
+// console.log( typeof value );
+
+// console.log( unDef );
+// console.log( typeof unDef );
+
+// Variables
+const x = 19;
+const y = 10;
+const z = x + y;
+const fName = "John";
+
+console.log("Hello! " + fName); // Concatenation
+console.log("Result: " + z); // Concatenation
+console.log(`Result: ${z}`); // Template literal , Template string
+
+console.log(`Subtraction: ${x - y}`);
+console.log(`Multiplication: ${x * y}`);
+console.log(`Division: ${x / y}`);
+console.log(`Modulo: ${x % y}`); // Remainder operator
+// console.log("Modulo: " + (x % y) );
+
+let counter = 20;
+
+console.log(`Increment: ${++counter}`); // Increment operator
+console.log(counter);
+console.log(`Decrement: ${--counter}`); // Decrement operator
+
