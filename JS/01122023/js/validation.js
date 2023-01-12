0b4424d1af22e1725483f315ffc3eec7a726d791
john-kodego <101509551+john-kodego@users.noreply.github.com>
2023-01-12 15:31:27 +0800

    JS validation pt.2

--- JS/01122023/js/validation.js
+++ JS/01122023/js/validation.js
@@ -1,19 +1,59 @@
 const register = document.querySelector('#register');
 const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password')
+const confirmPassword = document.querySelector('#confirmPassword')
+
+const usernameError = document.querySelector('#usernameError');
+const emailError = document.querySelector('#emailError');
+const passwordError = document.querySelector('#passwordError');
+const confirmPasswordError = document.querySelector('#confirmPasswordError');
+
+const validateUsername = () => {
+    const userValue = username.value.trim();
+    usernameError.innerText = "";    
+    if( !userValue ) {
+        usernameError.innerText = 'Username is required';
+    }
+    // username should be between 5 to 16 characters
+    else if( userValue.length < 5 || userValue.length > 16 ) {
+        usernameError.innerText = 'Username must be between 5 to 16 characters';
+    }
+}
+const validateEmail = () => {
+    const emailValue = email.value.trim();
+    const emailValidator = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    emailError.innerText = "";
+
+    if( !emailValue ) {
+        emailError.innerText = 'Email is required';
+    } 
+    else if( !emailValidator.test(emailValue) ) {
+        emailError.innerText = 'Please enter a valid email';
+    }
+}
+
+const validatePassword = () => {
+    /*
+        1. Password is required
+        2. Password rules / pattern:
+            contains a digit, a symbol, 
+            at least 1 Uppercase letter, at least 5 characters
+    */
+}
+
+const validateConfirmPassword = () => {
+    /*
+        Password and Confirm password should be equal / same 
+    */
+}
 
 register.addEventListener(
     'submit',
     (e) => {
         e.preventDefault();
 
-        const userValue = username.value.trim();
-        
-        if( !userValue ) {
-            console.log('Username is required');
-        }
-        // username should be between 5 to 16 characters
-        else if( userValue.length < 5 || userValue.length > 16 ) {
-            console.log('Username must be between 5 to 16 characters');
-        }
+        validateUsername();
+        validateEmail();
     }
 )
