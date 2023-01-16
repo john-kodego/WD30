ead5b1d4bbd8c70b1ac2129531ce9873883af220
john-kodego <101509551+john-kodego@users.noreply.github.com>
2023-01-16 16:07:59 +0800

    Web API pt.2

--- JS/01162023/js/scripts.js
+++ JS/01162023/js/scripts.js
@@ -36,19 +36,68 @@ console.log( isRightTriangle(3, 4, 5) ); // true
 // Promise(s)
 // Resolved or Rejected
 
-loadFBNewsFeed('myProfile')
-    // res = response
-    .then((res) => {
-        // runs if promise is resolved
-    })
-    .then((res) => {
-        // runs if promise is resolved
-    })
-    .catch((err) => {
-        // runs if promise is rejected
-    })
+// loadFBNewsFeed('myProfile')
+//     // res = response
+//     .then((res) => {
+//         // runs if promise is resolved
+//     })
+//     .then((res) => {
+//         // runs if promise is resolved
+//     })
+//     .catch((err) => {
+//         // runs if promise is rejected
+//     })
     
 // Unsplash Access Key
-// YOUR_UNSPLASH_ACCESS_KEY
+// YOUR_ACCESS_KEY
 // https://api.unsplash.com/
+// /search/photos
 // Authorization: Client-ID YOUR_ACCESS_KEY
+
+// https://api.unsplash.com/search/photos?query=food
+// ?key=value&key2=value2&key3=value3
+// query string / query parameters
+
+const baseURL = "https://api.unsplash.com/";
+const search = "search/photos";
+const query = "?query=landscape";
+const requestURL = baseURL + search + query;
+// https://api.unsplash.com/search/photos?query=cars
+
+const ACCESS_KEY = "wGvg3zZtaWCZZHVyMmHVz89smUCwOeuOF_AnuUiQGPs";
+const clientID = `Client-ID ${ACCESS_KEY}`;
+
+// create headers
+const myHeaders = new Headers();
+myHeaders.append("Authorization", clientID);
+
+// request options / configuration
+const requestOptions = {
+    method: "GET",
+    headers: myHeaders
+}
+
+const output = document.querySelector('#output');
+
+fetch( requestURL, requestOptions )
+    .then(res => {
+        return res.json();
+    })
+    .then(result => {
+        console.log(result);
+
+        for ( let photo of result.results ) {
+            // photo.urls.small
+            const imageContainer = document.createElement('div');
+            const image = document.createElement('img');
+
+            image.src = photo.urls.small;
+
+            imageContainer.append(image);
+            output.append(imageContainer);
+        }
+
+    })
+    .catch(err => {
+        console.log(err);
+    })
